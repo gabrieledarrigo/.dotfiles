36252f1a01e3d4b7f1345e576f22f191f5a6af12
Gabriele D'Arrigo <darrigo.g@gmail.com>
2020-04-25 14:31:25 +0200

    Update extensions.sh
    Specify only a minimal set of extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -4,11 +4,5 @@
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
 code --install-extension jpoissonnier.vscode-styled-components
-code --install-extension mikestead.dotenv
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension PeterJausovec.vscode-docker
 code --install-extension PKief.material-icon-theme
-code --install-extension zhuangtongfa.Material-theme
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension tlent.jest-snapshot-language-support
-code --install-extension ms-vscode.cpptools
